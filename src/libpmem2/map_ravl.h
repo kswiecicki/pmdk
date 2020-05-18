c868746f739117071e2b332ec01152592b8e4901
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2020-05-18 11:58:07 +0200

    pmem2: separate ravl map module and map.c
    Move ravl map module implementation from 'map.c' into separate files
    'map_ravl.c' and 'map_ravl.h'.
    Effectively 'map.c' uses 'map_ravl' structure to keep track
    of the mappings in the ravl tree.

---
+++ src/libpmem2/map_ravl.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * map_ravl.h -- internal definitions for map_ravl
+ */
+
+#ifndef MAP_RAVL_H
+#define MAP_RAVL_H
+
+#include "os_thread.h"
+#include "ravl.h"
+
+struct map_ravl {
+	struct ravl *tree;
+	os_rwlock_t lock;
+};
+
+int map_ravl_new(struct map_ravl **mr);
+void map_ravl_delete(struct map_ravl **mr);
+struct pmem2_map *map_ravl_find(struct map_ravl **mr, const void *addr,
+		size_t size);
+int map_ravl_add(struct map_ravl **mr, struct pmem2_map *map);
+int map_ravl_remove(struct map_ravl **mr, struct pmem2_map *map);
+
+#endif
